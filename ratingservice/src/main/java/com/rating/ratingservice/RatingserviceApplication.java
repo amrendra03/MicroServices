c55ae3a3134fb1e6cbafa79ae43e49fc8b7bad39
amrendra03 <amrendra9076893685@gmail.com>
2023-12-18 17:56:08 +0530

    RatingService-init

---
+++ ratingservice/src/main/java/com/rating/ratingservice/RatingserviceApplication.java
@@ -0,0 +1,14 @@
+package com.rating.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingserviceApplication.class, args);
+		System.out.println("rating service started...");
+	}
+
+}
---
+++ ratingservice/src/test/java/com/rating/ratingservice/RatingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rating.ratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
