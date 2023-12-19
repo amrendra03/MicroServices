bf386ed1935bd879903b06e9e2ea1ce0710b664c
amrendra03 <amrendra9076893685@gmail.com>
2023-12-19 08:55:45 +0530

    RatingService-Created-Client-Discovery

--- ratingservice/src/main/java/com/rating/ratingservice/RatingserviceApplication.java
+++ ratingservice/src/main/java/com/rating/ratingservice/RatingserviceApplication.java
@@ -2,8 +2,10 @@ package com.rating.ratingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RatingserviceApplication {
 
 	public static void main(String[] args) {
