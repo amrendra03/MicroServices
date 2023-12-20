a1b216817e006fe39c93e8187b08ef1a58580901
amrendra03 <amrendra9076893685@gmail.com>
2023-12-20 23:28:52 +0530

    Fetching-Rating-Data-in-userservice-RestTemplate

--- userservice/src/main/java/com/userservice/userservice/services/impl/UserServiceImpl.java
+++ userservice/src/main/java/com/userservice/userservice/services/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.userservice.userservice.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.userservice.userservice.entities.User;
 import com.userservice.userservice.exception.ResourceNotFoundException;
@@ -16,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
    @Autowired
    private UserRepo userRepo;
+
+   @Autowired
+   private RestTemplate restTemplate;
+
+   private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
    @Override
    public User saveUser(User user) {
@@ -32,8 +41,13 @@ public class UserServiceImpl implements UserService {
    @Override
    public User getUser(String userId) {
 
-      return this.userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+      User user = this.userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
+      ArrayList arr = restTemplate
+            .getForObject("http://localhost:8083/ratings/users/12b688ac-7d4c-4ffd-ac54-799767829436", ArrayList.class);
+
+      logger.info("{}", arr);
+      return user;
    }
 
 }
