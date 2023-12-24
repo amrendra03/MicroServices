e189783b998ac2665e636f06f791c8a94efa7fd6
amrendra03 <amrendra9076893685@gmail.com>
2023-12-24 22:10:45 +0530

    Config-server-m

--- ConfigServer/src/main/java/com/config/service/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/config/service/ConfigServer/ConfigServerApplication.java
@@ -2,12 +2,15 @@ package com.config.service.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("Form Config Server Started...");
 	}
 
 }
